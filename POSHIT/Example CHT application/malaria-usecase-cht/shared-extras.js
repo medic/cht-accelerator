d87cd783a27c3d494d51a0107799787575e84caa
brian-gacheru <brian.gacheru@poshit.co.ke>
2023-06-08 15:53:28 +0300

    added household contact summary

---
+++ POSHIT/Example CHT application/malaria-usecase-cht/shared-extras.js
@@ -0,0 +1,10 @@
+const CONTACT_TYPES = {
+  HOUSEHOLD: 'household',
+  HOUSEHOLD_MEMBER: 'household_member',
+  COMMUNITY_HEALTH_AREA: 'community_health_area',
+  COMMUNITY_HEALTH_VOLUNTEER: 'community_health_volunteer'
+};
+  
+module.exports = {
+  CONTACT_TYPES
+};
