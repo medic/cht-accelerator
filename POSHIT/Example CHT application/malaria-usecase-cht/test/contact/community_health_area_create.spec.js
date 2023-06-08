22aca90e58304e065a6eb5e8c8858c97fedbca96
brian-gacheru <brian.gacheru@poshit.co.ke>
2023-06-08 16:00:01 +0300

    added contact forms for CHA and CHV along wth tests

---
+++ POSHIT/Example CHT application/malaria-usecase-cht/test/contact/community_health_area_create.spec.js
@@ -0,0 +1,24 @@
+const { expect } = require('chai');
+const TestRunner = require('cht-conf-test-harness');
+const { communityHealthArea } = require('../form-inputs');
+const { CONTACT_TYPES } = require('../../shared-extras');
+const harness = new TestRunner();
+describe('Community Health Area registration form', () => {
+  before(() => harness.start());
+  after(() => harness.stop());
+  beforeEach(
+    async () => {
+      await harness.clear();
+    });
+  afterEach(() => expect(harness.consoleErrors).to.be.empty);
+  it('should create a community health area with the correct details', async() => {
+    const result = await harness.fillContactCreateForm(CONTACT_TYPES.COMMUNITY_HEALTH_AREA, ...communityHealthArea.ok);
+    expect(result.errors).to.be.empty;
+    expect(result.contacts).to.have.lengthOf(2);
+    expect(result.contacts.filter((contact) => contact.contact_type === CONTACT_TYPES.COMMUNITY_HEALTH_AREA).length === 1).to.be.true;
+    expect(result.contacts[1]).to.deep.include({
+      name: communityHealthArea.ok[0][1],
+      phone: communityHealthArea.ok[0][2]
+    });
+  });
+});
---
+++ POSHIT/Example CHT application/malaria-usecase-cht/test/contact/community_health_volunteer_create.spec.js
@@ -0,0 +1,26 @@
+const { expect } = require('chai');
+const TestRunner = require('cht-conf-test-harness');
+const { communityHealthVolunteer } = require('../form-inputs');
+const { CONTACT_TYPES } = require('../../shared-extras');
+const harness = new TestRunner();
+describe('Community Health Volunteer registration form', () => {
+  before(() => harness.start());
+  after(() => harness.stop());
+  beforeEach(
+    async () => {
+      await harness.clear();
+    });
+  afterEach(() => expect(harness.consoleErrors).to.be.empty);
+  it('should successfully create community health Volunteer with correct details', async() => {
+    const result = await harness.fillContactCreateForm(CONTACT_TYPES.COMMUNITY_HEALTH_VOLUNTEER, ...communityHealthVolunteer.ok);
+    expect(result.errors).to.be.empty;
+    expect(result.contacts).to.have.lengthOf(1);
+    expect(result.contacts.filter((contact) => contact.contact_type === CONTACT_TYPES.COMMUNITY_HEALTH_VOLUNTEER).length === 1).to.be.true;
+    expect(result.contacts[0]).to.deep.include({
+      name: communityHealthVolunteer.ok[0][0],
+      gender: communityHealthVolunteer.ok[0][1],
+      dob: communityHealthVolunteer.ok[0][2],
+      primary_phone: communityHealthVolunteer.ok[0][3]
+    });
+  });
+});
